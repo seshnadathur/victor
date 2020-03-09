c2be509f3566fc8d30a34ed9ee7e6f4ae09177a6
seshnadathur <39797132+seshnadathur@users.noreply.github.com>
2020-03-09 19:47:01 +0000

    added non flat background calculator

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -6,13 +6,15 @@ import matplotlib as mpl
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap, colorConverter
 from scipy.ndimage import gaussian_filter
 import os
+from scipy import constants
+from scipy.special import legendre
 
 
 class Cosmology:
 
-    def __init__(self, omega_m=0.308, h=0.676):
+    def __init__(self, omega_m=0.31, h=0.676):
         # print('Initializing cosmology with omega_m = %.3f' % omega_m)
-        c = 299792.458
+        c = constants.c / 1000
         omega_l = 1.-omega_m
         ztab = np.linspace(0, 4, 1000)
         rtab = np.zeros_like(ztab)
@@ -29,6 +31,45 @@ class Cosmology:
     # comoving distance in Mpc/h
     def get_comoving_distance(self, z):
         return np.interp(z, self.ztab, self.rtab)
+
+    def get_redshift(self, r):
+        return np.interp(r, self.rtab, self.ztab)
+
+    def get_ez(self, z):
+        return 100 * np.sqrt(self.omega_m * (1 + z)**3 + self.omegaL)
+
+    def get_hubble(self, z):
+        return self.h * self.get_ez(z)
+
+
+class NonFlatBackground:
+
+    def __init__(self, omega_m=0.31, omega_l=0.69, h=0.676):
+        # print('Initializing cosmology with omega_m = %.3f' % omega_m)
+        omega_k = 1. - omega_m - omega_l
+        ztab = np.linspace(0, 4, 1000)
+        rtab = np.zeros_like(ztab)
+        for i in range(len(ztab)):
+            rtab[i] = quad(lambda x: 0.01 * (const.c / 1000) \
+            / np.sqrt(omega_m * (1 + x)**3 + omega_k * (1 + x)**2 + omega_l), 0, ztab[i])[0]
+
+        self.h = h
+        self.omega_m = omega_m
+        self.omegaL = omega_l
+        self.omegaK = omega_k
+        self.ztab = ztab
+        self.rtab = rtab
+
+    # comoving distance in Mpc/h
+    def get_comoving_distance(self, z):
+        return np.interp(z, self.ztab, self.rtab)
+
+    def get_ez(self, z):
+        return 100 * np.sqrt(self.omega_m * (1 + z)**3 +
+                             self.omegaK * (1 + z)**2 + self.omegaL)
+
+    def get_hubble(self, z):
+        return self.h * self.get_ez(z)
 
     def get_redshift(self, r):
         return np.interp(r, self.rtab, self.ztab)
@@ -116,6 +157,28 @@ class UtilMethods:
                         output_cm[k, i + k - bin_range, j + k - bin_range] = input_cm[k, i, j]
 
         return output_cm
+
+    @staticmethod
+    def correlation_multipoles(xirmu, r, ell=0):
+        """
+        Calculate the Legendre multipoles of a correlation function xi(r, mu)
+
+        :param xirmu: an instance of scipy.interpolate.interp2d providing an interpolation for xi(r, mu)
+        :param r: array of r values at which to calculate the multipoles
+        :param ell: integer, multipole order
+
+        Note that xi(r, mu) is assumed an even function of mu, so integral is evaluated over 0<mu<1 and then doubled
+
+        Returns:
+                array of values at the input r
+        """
+
+        output = np.zeros(len(r))
+        lmu = legendre(ell)
+        for i in range(len(r)):
+                output[i] = quad(lambda x: xirmu(r[i], x) * lmu(x) * (2 * ell + 1), 0, 1, full_output=1)[0]
+
+        return output
 
     @staticmethod
     def quadrupole(xirmu, mu):
@@ -265,7 +328,7 @@ class FigureUtilities:
                   Defaults to 1.0 (no upper ofset). Should be between
                   `midpoint` and 1.0.
         """
-        
+
         cdict = {
             'red': [],
             'green': [],
@@ -350,4 +413,3 @@ class FigureUtilities:
         contour_kwargs = dict()
         contour_kwargs["colors"] = contour_kwargs.get("colors", color2)
         plt.contour(X2, Y2, H2.T, V, **contour_kwargs)
-
