2d14dfa58c29fe7f3909105faa1524e71376533d
Sesh Nadathur <s.nadathur@ucl.ac.uk>
2021-04-06 17:42:12 +0100

    allow direct settings/paths yaml input if desired

--- likelihoods/VoidGalaxyLikelihood.py
+++ likelihoods/VoidGalaxyLikelihood.py
@@ -9,10 +9,16 @@ class VoidGalaxyLikelihood(Likelihood):
         """
 
         """
-        with open(self.spec_file) as file:
-            info = yaml.full_load(file)
-        self.settings = info['settings']
-        self.paths = info['paths']
+        if self.settings is None or self.paths is None:
+            # these are then to be read from the separate spec_file provided
+            try:
+                with open(self.spec_file) as file:
+                    info = yaml.full_load(file)
+                self.settings = info['settings']
+                self.paths = info['paths']
+            except:
+                raise KeyError('If settings and paths not provided in input yaml, spec_file must point to valid file with this info')
+
         self.vgfitter = VoidGalaxyCCF(self.paths, self.settings)
 
     def logp(self, **params_values):
